9dc9c61ceb5670188c978e27a848f669142808db
kimsia <kimsia@kimsiaui-MacBookAir.local>
2022-10-01 12:12:02 +0900

    feat: error modal UI 구현

---
+++ client/src/components/atoms/modal/Error.tsx
@@ -0,0 +1,22 @@
+import styles from '../../../styles/Modal.module.scss';
+
+const ErrorModal = () => {
+    return (
+        <div className={styles.background}>
+            <div className={styles.content}>
+                <div className={styles.desc_area}>
+                    <img src="/images/alert.svg" alt="alert" />
+                    <div className={styles.desc}>
+                        <span>시스템 에러가 발생하였습니다.</span>
+                        <span>잠시 후 다시 시도해주세요.</span>
+                    </div>
+                </div>
+                <button type="button" className={styles.button}>
+                    확인
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default ErrorModal;
