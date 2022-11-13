ef68c629c9fdcb2ca334a699ba808fb6b664a4fe
kimsia <kimsia@kimsiaui-MacBookAir.local>
2022-11-13 13:20:58 +0900

    feat: 메인 페이지 UI 구현 초안

---
+++ client/src/containers/Home.tsx
@@ -0,0 +1,11 @@
+import HomeComponent from '../components/home/Home';
+
+const HomeContainer = () => {
+    return (
+        <div id="container">
+            <HomeComponent />
+        </div>
+    );
+};
+
+export default HomeContainer;
