7908f3eaada3ed3c8e4bdad3ceee90319d244411
kimsia <kimsia@kimsiaui-MacBookAir.local>
2022-11-26 13:49:54 +0900

    feat: 네비게이션바 구현

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,9 @@
 import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
-import { Route, Routes, Navigate } from 'react-router-dom';
-import { Main, Join, JoinPlanting, Home } from './pages/index';
+import { Route, Routes, Navigate, useLocation } from 'react-router-dom';
 import { RootState } from './modules';
+import { Main, Join, JoinPlanting, Home } from './pages/index';
+import Navigation from './components/atoms/Navigation';
 import './App.css';
 
 const setScreenSize = () => {
@@ -11,20 +12,27 @@ const setScreenSize = () => {
 };
 
 const App = () => {
+    const location = useLocation();
+
     const { isSigning }: any = useSelector((state: RootState) => state.user);
+
+    const noHeaderPages = ['/', '/join', '/login', '/plant-seeds/1', '/plant-seeds/2', '/plant-seeds/3', '/plant-seeds/4'];
 
     useEffect(() => {
         setScreenSize();
     }, []);
 
     return (
-        <Routes>
-            <Route path="/" element={<Main />} />
-            <Route path="/join" element={<Join />} />
-            <Route path="/login" element={<Join />} />
-            <Route path="/plant-seeds/:state" element={isSigning ? <JoinPlanting /> : <Navigate to='/join' />} />
-            <Route path="/home" element={<Home />} />
-        </Routes>
+        <>
+            <Routes>
+                <Route path="/" element={<Main />} />
+                <Route path="/join" element={<Join />} />
+                <Route path="/login" element={<Join />} />
+                <Route path="/plant-seeds/:state" element={isSigning ? <JoinPlanting /> : <Navigate to='/join' />} />
+                <Route path="/home" element={<Home />} />
+            </Routes>
+            {!(noHeaderPages.includes(location.pathname)) && <Navigation />}
+        </>
     );
 };
 
