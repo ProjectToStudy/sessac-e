d38f2d505c0178d2c564415275af449e2a8a3935
kimsia <kimsia@kimsiaui-MacBookAir.local>
2022-12-20 22:13:19 +0900

    design: 스터디 찾기 페이지 UI 구현

--- client/src/components/study/Study.tsx
+++ client/src/components/study/Study.tsx
@@ -21,6 +21,56 @@ const testData = [
         number: 17,
         like: false,
         sticker: 'hot',
+    },
+    {
+        img: '',
+        mainCategory: '자격증',
+        subCategory: '토익',
+        title: '정기적인 스터디로 토익점수 올리자!',
+        date: '12월 10일 ~ 6월 30일',
+        number: 17,
+        like: false,
+        sticker: 'hot',
+    },
+    {
+        img: '',
+        mainCategory: '자격증',
+        subCategory: '토익',
+        title: '정기적인 스터디로 토익점수 올리자!',
+        date: '12월 10일 ~ 6월 30일',
+        number: 17,
+        like: false,
+        sticker: 'hot',
+    },
+    {
+        img: '',
+        mainCategory: '자격증',
+        subCategory: '토익',
+        title: '정기적인 스터디로 토익점수 올리자!',
+        date: '12월 10일 ~ 6월 30일',
+        number: 17,
+        like: false,
+        sticker: 'hot',
+    },
+    {
+        img: '',
+        mainCategory: '자격증',
+        subCategory: '토익',
+        title: '정기적인 스터디로 토익점수 올리자!',
+        date: '12월 10일 ~ 6월 30일',
+        number: 17,
+        like: false,
+        sticker: 'hot',
+    },
+    {
+        img: '',
+        mainCategory: '자격증',
+        subCategory: '토익',
+        title: '정기적인 스터디로 토익점수 올리자!',
+        date: '12월 10일 ~ 6월 30일',
+        number: 17,
+        like: false,
+        sticker: 'hot',
     }
 ]
 
@@ -39,15 +89,15 @@ const StudyItem = ({ props }: { props: StudyItemProps }) => {
     const { img, mainCategory, subCategory, title, date, number, recommend } = props;
     return (
         <li className={styles.item}>
-            <div>
-                <img src={img} alt="image" />
+            <div className={styles.thumbnail_area}>
+                <img src={img} alt="image" className={styles.thumbnail} />
             </div>
-            <div>
-                <span>{mainCategory} &gt; {subCategory}</span>
-                <p>{title}</p>
-                <span>{date}</span>
-                <span>총 {number}회 진행</span>
-                {recommend && <span>[맞춤추천] {recommend}</span>}
+            <div className={styles.info}>
+                <span className={styles.category}>{mainCategory} &gt; {subCategory}</span>
+                <p className={styles.title}>{title}</p>
+                <span className={styles.date}>{date}</span>
+                <span className={styles.number}>총 {number}회 진행</span>
+                {recommend && <p className={styles.recommend}><b>[맞춤추천]</b> {recommend}</p>}
             </div>
         </li>
     );
@@ -65,7 +115,7 @@ const StudyList = ({ studyList }: { studyList: StudyItemProps[] }) => {
 
 const StudyComponent = () => {
     return (
-        <div id="component">
+        <div id="component" className={styles.component}>
             <StudyList studyList={testData} />
         </div>
     );
