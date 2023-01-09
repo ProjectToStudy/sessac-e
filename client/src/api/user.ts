193b6a0eb1ce16d380d6ebe470fe3ab85a1ebfdf
kimsia <kimsia@kimsiaui-MacBookAir.local>
2023-01-09 13:17:15 +0900

    refactor: access token 저장 로직 수정

--- client/src/api/user.ts
+++ client/src/api/user.ts
@@ -1,11 +1,11 @@
 import Axios from './.';
-import { getCookie } from '../utils/cookie';
+import { getAccessToken } from '../utils/cookie';
 
 export const certSend = (phone: string) => Axios.post('user/cert/test', { phone });
 export const certCheck = ({ phone, certificationNumber }: { phone: string, certificationNumber: string }) =>
     Axios.post('/user/cert/check', { phone: phone.replaceAll(' ', ''), certificationNumber });
 
 export const login = (phone: string) => Axios.post('/user/login', { phone }, { withCredentials: true });
-export const join = (phone: string) => Axios.post('/user/join', { phone });
+export const join = (phone: string) => Axios.post('/user/join', { phone }, { withCredentials: true });
 export const update = ({ career, purpose, etc }: { career: string[], purpose: string[], etc: string }) =>
-    Axios.patch('/user', { data: { career, purpose, etc } }, { headers: { Authorization: `Bearer ${getCookie('at')}` } });
+    Axios.patch('/user', { data: { career, purpose, etc } }, { headers: { Authorization: `Bearer ${getAccessToken()}` } });
