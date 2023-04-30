345e4e01fae3445922d6be78fbdc7684572cd2b7
kimsia <kimsia@kimsiaui-MacBookAir.local>
2023-04-30 12:33:28 +0900

    fix: bugfix 및 수정사항 반영

--- client/src/containers/study/Study.tsx
+++ client/src/containers/study/Study.tsx
@@ -5,6 +5,7 @@ import { categoryListAPI } from '../../modules/study';
 import { getStudy } from '../../api/study';
 import StudyComponent, { StudyItemType } from '../../components/study/Study';
 import { userStudy } from '../../api/user';
+import { getAccessToken } from '../../utils/cookie';
 
 const StudyContainer = () => {
     const dispatch = useDispatch();
@@ -34,7 +35,7 @@ const StudyContainer = () => {
     useEffect(() => {
         if (!categoryList.length) dispatch(categoryListAPI());
         getStudyList();
-        getUserStudyInfo();
+        if (getAccessToken()) getUserStudyInfo();
     }, []);
 
     return (
