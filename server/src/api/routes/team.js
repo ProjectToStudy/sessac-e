8ba6ed06e93df3c6e4f95ed4d6fc68753546d47c
hyesu <peche3@naver.com>
2023-05-13 16:21:18 +0900

    chore: sms 환경변수 설정, swagger 코드 제거

--- server/src/api/routes/team.js
+++ server/src/api/routes/team.js
@@ -2,7 +2,6 @@ const express = require('express');
 
 const route = express.Router();
 
-//@TODO: team.js 의 경우 기존의 user.js 와 구조가 조금 다름 -> 추후 통일
 const teamController = require('../../controllers/team');
 const authMiddleware = require('../../middleware/auth');
 
@@ -11,56 +10,6 @@ const teamRouter = ({ app }) => {
 
     route.get('', teamController.getTeams);
 
-    /**
-     * @swagger
-     * /api/v1/teams/categories:
-     *  get:
-     *      tags:
-     *          - team
-     *      description: 팀 카테고리 get
-     *      parameters:
-     *         - in: query
-     *           name: id
-     *           required: false
-     *           schema:
-     *              type: string
-     *           description: 카테고리 id
-     *         - in: query
-     *           name: type
-     *           required: false
-     *           schema:
-     *             type: string
-     *           description: 카테고리 타입
-     *         - in: query
-     *           name: isValid
-     *           required: false
-     *           schema:
-     *             type: string
-     *           description: 카테고리 유효성 여부
-     *      responses:
-     *       200:
-     *         description: 성공
-     *         content:
-     *           application/json:
-     *             example:
-     *               - code: 200000
-     *                 message: success
-     *                 result: []
-     *       400:
-     *         description: 실패
-     *         content:
-     *           application/json:
-     *             example:
-     *               - code: 400666
-     *                 message: 데이터 get 실패
-     *       500:
-     *         description: 실패
-     *         content:
-     *           application/json:
-     *             example:
-     *               - code: 500000
-     *                 message: 서버 에러 발생
-     */
     route.get('/categories', teamController.getCategories);
 
     route.post('/stats', authMiddleware.verify, teamController.postStats);
